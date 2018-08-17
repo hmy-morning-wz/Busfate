b5de4cf64b8ceb2301f9c42cb305cce1f89a4575
hongmy <hongmy@citytsm.com>
2018-08-17 14:20:47 +0800

    up

--- src/components/result.vue
+++ src/components/result.vue
@@ -1,28 +1,23 @@
 <template>
-    <div class="result">
-    </div>
+  <div class="result">
+    <div class="result-page"></div>
+  </div>
 </template>
+
 <script>
 export default {
+  name: 'Result',
   data() {
-    return {
-    }
-  },
-  components: {},
-  created() {
-  },
-  computed: {},
-  methods: {
-  },
-  mounted() {},
-  watch: {}
+    return {}
+  }
 }
 </script>
-<style lang="scss" scoped >
-.result {
-    width: 100%;
-    background: url("../assets/images/result.png") no-repeat;
-    height: 40.026667rem;
-    background-size: cover;
+
+<style lang="scss">
+.result-page{
+  width: -webkit-fill-available;
+  height: 40rem;
+  background: url('../assets/images/result.png') no-repeat;
+  background-size: contain;
 }
 </style>
